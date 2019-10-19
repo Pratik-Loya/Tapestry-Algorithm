e5c43a860dc68309c70d28a229fbb0b2cae0f581
kdlogan19 <kirtikd19@gmail.com>
2019-10-19 14:46:18 -0400

    Filling table according to distance between nodes

--- tapestry_algorithm/lib/create_node.ex
+++ tapestry_algorithm/lib/create_node.ex
@@ -1,7 +1,7 @@
 defmodule CreateNode do
 
     @crypto_function :sha
-    @no_of_bits 8
+    @no_of_bits 4
 
     def start_link() do
         GenServer.start_link(__MODULE__, [], name: :create_node)
@@ -12,26 +12,27 @@ defmodule CreateNode do
     end
 
     def handle_call({:create_network, num_nodes}, _from, state) do
-        node_list = Enum.reduce(1..num_nodes, [], fn number,bucket-> 
+        node_list = Enum.reduce(1..num_nodes, [], fn number,bucket->
             hash_value = get_hash_value(number,num_nodes,bucket)
             [hash_value | bucket]
         end)
-        '''
+        
         node_list = ["1001","5CFE","1222","FEBC","1235","1211","1167","1F98","11BC","1BDF"]
         node_hash = "1234"
         NetworkNode.start(node_list,node_hash)
-        '''
+
         #NetworkNode.start(node_list,Enum.at(node_list,0))
-        
-        Enum.each(node_list, fn node_hash-> 
+        '''
+        Enum.each(node_list, fn node_hash->
             NetworkNode.start(node_list,node_hash)
             #IO.inspect node_hash
             #IO.inspect getPid(node_hash)
         end)
-        
+        '''
+
         {:reply, state,state}
     end
-    
+
 
     def getPid(node_id) do
         case Registry.lookup(:registry, node_id) do
--- tapestry_algorithm/lib/network_node.ex
+++ tapestry_algorithm/lib/network_node.ex
@@ -7,7 +7,7 @@ defmodule NetworkNode do
         #routing table for 4 levels
         routing_table = %{
             1 => %{0 => "",1 => "",2 => "",3 => "",4 => "",5 => "",6 => "",7 => "",8 => "",9 => "",'a' => "",'b' => "",'c' => "",'d' => "",'e' => "",'f' => ""},
-            2 => %{0 => "",1 => "",2 => "",3 => "",4 => "",5 => "",6 => "",7 => "",8 => "",9 => "",'a' => "",'b' => "",'c' => "",'d' => "",'e' => "",'f' => ""},    
+            2 => %{0 => "",1 => "",2 => "",3 => "",4 => "",5 => "",6 => "",7 => "",8 => "",9 => "",'a' => "",'b' => "",'c' => "",'d' => "",'e' => "",'f' => ""},
             3 => %{0 => "",1 => "",2 => "",3 => "",4 => "",5 => "",6 => "",7 => "",8 => "",9 => "",'a' => "",'b' => "",'c' => "",'d' => "",'e' => "",'f' => ""},
             4 => %{0 => "",1 => "",2 => "",3 => "",4 => "",5 => "",6 => "",7 => "",8 => "",9 => "",'a' => "",'b' => "",'c' => "",'d' => "",'e' => "",'f' => ""}
             #5 => %{0 => "",1 => "",2 => "",3 => "",4 => "",5 => "",6 => "",7 => "",8 => "",9 => "",'a' => "",'b' => "",'c' => "",'d' => "",'e' => "",'f' => ""},
@@ -36,15 +36,28 @@ defmodule NetworkNode do
     end
 
     def get_row_map(row_map,nth_row_list,node_hash,row_num,-1), do: row_map
-        
+
     def get_row_map(row_map,nth_row_list,node_hash,row_num,col_num) do
+        #IO.inspect nth_row_list
         hex_value = "0123456789ABCDEF"
         #finding list of values eligible for 1 column
         value = Enum.filter(nth_row_list, fn(x) ->  String.at(x,row_num-1) == String.at(hex_value,col_num) end)
 
         #find distance
+        int_hashvalue = Integer.parse(node_hash,16)
+
+        IO.inspect value, label: "value of row"
+
+        if(value != []) do
+          int_value = Enum.min_by(Enum.map(value, fn x -> {x, List.to_integer(x |> to_charlist(),16) - int_hashvalue } end), fn({x,y}) -> abs(x) end)
+          #IO.inspect int_value, label: "if part cleared"
+          row_map = put_in(row_map[String.at(hex_value,col_num)],int_value)
+        else
+          IO.puts "in else part"
+          row_map = put_in(row_map[String.at(hex_value,col_num)],value)
+        end
         '''
-        value = Enum.filter(nth_row_list,[], fn(x,acc) -> 
+        value = Enum.filter(nth_row_list,[], fn(x,acc) ->
             last_char = String.at(String.slice(x,0,row_num),-1)
             if(get_in(row_map,[last_char]) == "" ) do
                 row_map = put_in(row_map[last_char], x)
@@ -54,7 +67,7 @@ defmodule NetworkNode do
         end)
         '''
         #IO.inspect row_map
-        row_map = put_in(row_map[String.at(hex_value,col_num)],value)
+        #row_map = put_in(row_map[String.at(hex_value,col_num)],int_value)
         get_row_map(row_map,nth_row_list,node_hash,row_num,col_num-1)
     end
 end
