4b0c6f157060707f16543d8cf238cb9120d71d70
Pratik Loya <loyapratik@gmail.com>
2019-10-19 16:23:43 -0400

    routing table v1

--- tapestry_algorithm/lib/network_node.ex
+++ tapestry_algorithm/lib/network_node.ex
@@ -26,7 +26,7 @@ defmodule NetworkNode do
     def change_routing_table(routing_table,node_list,node_hash,row_num) do
         nth_row_list = Enum.filter(node_list,fn(nodes) -> String.slice(nodes,0,row_num-1)==String.slice(node_hash,0,row_num-1) end)
         #IO.inspect row_num
-        #IO.inspect nth_row_list
+        IO.inspect nth_row_list
         row_map = %{"0" => "","1" => "","2" => "","3" => "","4" => "","5" => "","6" => "","7" => "","8" => "","9" => "","A" => "","B" => "","C" => "","D" => "","E" => "","F" => ""}
         nth_row_map = get_row_map(row_map,nth_row_list,node_hash,row_num,15)
         #IO.inspect nth_row_map
@@ -38,36 +38,22 @@ defmodule NetworkNode do
     def get_row_map(row_map,nth_row_list,node_hash,row_num,-1), do: row_map
 
     def get_row_map(row_map,nth_row_list,node_hash,row_num,col_num) do
-        #IO.inspect nth_row_list
         hex_value = "0123456789ABCDEF"
         #finding list of values eligible for 1 column
         value = Enum.filter(nth_row_list, fn(x) ->  String.at(x,row_num-1) == String.at(hex_value,col_num) end)
 
         #find distance
-        int_hashvalue = Integer.parse(node_hash,16)
-
-        IO.inspect value, label: "value of row"
-
-        if(value != []) do
-          int_value = Enum.min_by(Enum.map(value, fn x -> {x, List.to_integer(x |> to_charlist(),16) - int_hashvalue } end), fn({x,y}) -> abs(x) end)
-          #IO.inspect int_value, label: "if part cleared"
-          row_map = put_in(row_map[String.at(hex_value,col_num)],int_value)
+        int_hashvalue = List.to_integer(node_hash |> to_char_list(),16)
+         if(value != []) do
+          {hash_value,_diff} = Enum.min_by(Enum.map(value, fn x -> {x, abs(List.to_integer(x |> to_charlist(),16) - int_hashvalue) } end), fn({x,y}) -> y end)
+          IO.inspect hash_value, label: "if part cleared"
+          row_map = put_in(row_map[String.at(hex_value,col_num)],hash_value)
+          get_row_map(row_map,nth_row_list,node_hash,row_num,col_num-1)
         else
-          IO.puts "in else part"
           row_map = put_in(row_map[String.at(hex_value,col_num)],value)
+          get_row_map(row_map,nth_row_list,node_hash,row_num,col_num-1)
         end
-        '''
-        value = Enum.filter(nth_row_list,[], fn(x,acc) ->
-            last_char = String.at(String.slice(x,0,row_num),-1)
-            if(get_in(row_map,[last_char]) == "" ) do
-                row_map = put_in(row_map[last_char], x)
-                IO.inspect x
-                [x | acc ]
-            end
-        end)
-        '''
         #IO.inspect row_map
-        #row_map = put_in(row_map[String.at(hex_value,col_num)],int_value)
-        get_row_map(row_map,nth_row_list,node_hash,row_num,col_num-1)
+        #row_map = put_in(row_map[String.at(hex_value,col_num)],value)
     end
 end
